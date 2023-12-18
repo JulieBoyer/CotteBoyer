606ba26be4708c91a484c5e6b2f22979b4be0ece
Agathe Cotte <agathecotte42@gmail.com>
2023-12-18 16:59:38 +0100

    question_1_agathe_18_12

--- projet.py
+++ projet.py
@@ -15,16 +15,42 @@ def sigmoid(x):
     theta = 5.0  # seuil
     return 1/(1+np.exp(-a*(x-theta)))-1/(1+np.exp(a*theta))
 
-x = np.linspace(-10,10)
-plt.figure()
-plt.plot(x,sigmoid(x))
-plt.grid(True)
-plt.xlabel('$x$')
-plt.title("Fonction d'activation")
-plt.show()
+def trace_sigmoïde():
+    x = np.linspace(-10,10)
+    plt.figure()
+    plt.plot(x,sigmoid(x))
+    plt.grid(True)
+    plt.xlabel('$x$')
+    plt.title("Fonction d'activation")
+    plt.show()
 
 
 
 ###                 Matrice d'interconnection
 W = np.array([[5.0, -4.0], [8.0, -1.0]])
-print(W)
+
+###                 Point d'équilibre
+def plot_zero_locus(d_ext):
+    grid_x1 = np.linspace(-0.1,2.0,100)
+    grid_x2 = np.linspace(-2.0,2.0,100)
+    X1, X2 = np.meshgrid(grid_x1,grid_x2)
+
+    X1dot = -X1 + sigmoid(W[0,0]*X1 + W[0,1]*X2 + d_ext[0])
+    X2dot = -X2 + sigmoid(W[1,0]*X1 + W[1,1]*X2 + d_ext[1])
+
+    plt.figure()
+    C1 = plt.contour(X1, X2, X1dot, 0, colors = 'red')
+    C2 = plt.contour(X1, X2, X2dot, 0, colors = 'green')
+    h1,_ = C1.legend_elements()
+    h2,_ = C2.legend_elements()
+    plt.grid(True)
+    plt.xlabel('$x_1$')
+    plt.ylabel('$x_2$')
+    plt.legend([h1[0],h2[0]],['$\dot x_1 = 0$','$\dot x_2 = 0$'])
+    plt.title("Zero locus for $d_{ext} =$ "+ str(d_ext))
+    plt.show()
+    return
+
+
+d_ext = np.array([0.0,0.0])  # A CHOISIR
+plot_zero_locus(d_ext)
