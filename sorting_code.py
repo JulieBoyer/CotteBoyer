395e5af5bbcca31dce091db7dbc2d61cc920fc2b
Agathe Cotte <agathecotte42@gmail.com>
2024-01-16 16:58:17 +0100

    commit

--- sorting_code.py
+++ sorting_code.py
@@ -1,5 +1,6 @@
 #bubble sort
-def bubble_sort(L):
+def bubble_sort(x):
+    L=x.copy()
     for i in range(len(L)-1):
         if L[i]>L[i+1]:
             L[i], L[i+1]=L[i+1],L[i]
@@ -7,22 +8,24 @@ def bubble_sort(L):
 
 #heap sort
 def heapify(L): #find the maximum of the list and puts it in first place
+    if L==[]:
+        return L
     place=0
     maxi=L[0]
     for i,w in enumerate(L):
         if w>maxi:
             place=i
-            maxi=L[0]
-    L[0],L[place]=L[0],L[place]
+            maxi=L[i]
+    L[0],L[place]=L[place],L[0]
 
 def heap_sort(L):
-    nb=len(L)
-    end=nb
-    heapify(L)
+    a=L.copy()
+    end=len(a)
+    heapify(a)
     while end>0:
         end=end-1
-        L[end],L[0]=L[0],L[end]
-        heapify(L)
-    return L
+        a[end],a[0]=a[0],a[end]
+        heapify(a[0:end-1])
+    return a
 
-print(heap_sort(["b","ab"]))
+print(heap_sort(["ab","AB","cb","cde","CDE"]))
