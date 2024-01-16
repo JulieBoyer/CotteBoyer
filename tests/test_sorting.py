8cfbbf11534b54b8096248155de82f1577d98e89
Julie Boyer <julie.boyer13@laposte.net>
2024-01-16 16:34:55 +0100

    heap_test

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -5,3 +5,12 @@ import sorting_code
 def test_bubble():
     assert sorting_code.bubble_sort(["ab","b"])==["ab","b"]
     assert sorting_code.bubble_sort(["ab","AB"])==["AB","ab"]
+    assert sorting_code.bubble_sort(["ab","AB","cb","cde","CDE"])==["AB","ab","cb","CDE","cde"]
+    assert sorting_code.bubble_sort(["agathe","cotte","julie","boyer"])==["agathe","boyer","cotte","julie"]
+    assert sorting_code.bubble_sort(["123","321","364","128"])==["123","128","321","364"]
+def test_head():
+    assert sorting_code.heap_sort(["ab","b"])==["ab","b"]
+    assert sorting_code.heap_sort(["ab","AB"])==["AB","ab"]
+    assert sorting_code.heap_sort(["ab","AB","cb","cde","CDE"])==["AB","ab","cb","CDE","cde"]
+    assert sorting_code.heap_sort(["agathe","cotte","julie","boyer"])==["agathe","boyer","cotte","julie"]
+    assert sorting_code.heap_sort(["123","321","364","128"])==["123","128","321","364"]
