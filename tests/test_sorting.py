1086a33b733d76dd5d0cc3db6ecad66273b247cc
Agathe Cotte <agathecotte42@gmail.com>
2024-01-16 16:19:08 +0100

    fin-bubble_sort

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -1,5 +1,7 @@
 #test_sorting
+#PYTHONPATH=.. python -m pytest .
 import sorting_code
 
 def test_bubble():
     assert sorting_code.bubble_sort(["ab","b"])==["ab","b"]
+    assert sorting_code.bubble_sort(["ab","AB"])==["AB","ab"]
