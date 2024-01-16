93de9f0a59f1ad80f6af525e03297f095d458146
Julie Boyer <julie.boyer13@laposte.net>
2024-01-16 17:02:05 +0100

    modified bubble

--- sorting_code.py
+++ sorting_code.py
@@ -1,9 +1,14 @@
 #bubble sort
 def bubble_sort(x):
     L=x.copy()
-    for i in range(len(L)-1):
-        if L[i]>L[i+1]:
-            L[i], L[i+1]=L[i+1],L[i]
+    n=len(L)
+    swapped = True
+    while swapped : 
+        swapped = False
+        for i in range(len(L)-1):
+            if L[i]>L[i+1]:
+                L[i], L[i+1]=L[i+1],L[i]
+                swapped = True
     return L
 
 #heap sort
