fe7cc47062e5c4c0238efdd0c03e933d93de97cf
Julie Boyer <julie.boyer13@laposte.net>
2024-01-23 14:56:07 +0100

    main function corrected

--- socket.py
+++ socket.py
@@ -28,10 +28,10 @@ def main():
     # create an INET, STREAMing socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # now connect to the web server on port 80 - the normal http port
-    s.connect(("local host", args.p))
+    s.connect(("localhost", args.p))
     # create an INET, STREAMing socket
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # bind the socket to a public host, and a well-known port
-    serversocket.bind((socket.gethostname(), 80))
+    serversocket.bind(("", args.p))
     # become a server socket
-    serversocket.listen(5)
+    serversocket.listen()
